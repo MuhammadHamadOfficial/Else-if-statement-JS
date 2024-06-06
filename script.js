76f501e6667683afcfdaa4d9b41edd2471069c21
MuhammadHamadOfficial <muhammadhamad03@gmail.com>
2024-06-06 12:01:17 +0500

    Project JS 1

---
+++ script.js
@@ -0,0 +1,17 @@
+var marks = prompt("Enter Your Marks");
+
+if (marks > 90) {
+    alert("Grade A");
+}
+else if (marks > 80) {
+    alert("Grade B");
+}
+else if (marks > 70) {
+    alert("Grade C");
+}
+else if (marks > 60) {
+    alert("Grade D");
+}
+else {
+    alert("You Are Fail");
+}
